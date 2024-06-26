97dc36b079a77aaea818e667a032dd0e19edd351
Adarsh Singh <adarshsince2004@gmail.com>
2024-06-26 16:31:08 +0530

    roadmap

---
+++ roadmap.js
@@ -0,0 +1,17 @@
+/*
+stsate is handled ny JS and UI is handled by DOM
+
+we wanted to make connection between js and dom
+
+
+we'll learn from projects
+
+react is a library
+
+LIBRARY:FLEXIBLE,COOL DUDE
+FRAMEWORK:RIGID,RULES,MILITARY
+
+TOPICS:state/UI/jsx,component reusability,props,hooks
+
+addon TOPICS:router,state management(redux,redux toolkit,context api),class based component,BAAS app)
+*/
