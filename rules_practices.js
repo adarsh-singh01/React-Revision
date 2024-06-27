3951d93df0bbe91ccd4acfcae7d4e673e995dbb1
Adarsh Singh <adarshsince2004@gmail.com>
2024-06-27 11:57:19 +0530

    rulesAndPractices

---
+++ rules_practices.js
@@ -0,0 +1,8 @@
+/*
+Vite Rules :
+component filename should be .jsx 
+function name should be first letter uppercase(in create-react-app also)
+
+Practices:
+keeping component file name first lettter uppercase
+*/
