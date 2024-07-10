2c4b4654fb9137496d8472461c5224ce0c07a93b
Adarsh Singh <adarshsince2004@gmail.com>
2024-07-11 00:01:56 +0530

    complete todo

--- toDoContextLocal/src/App.jsx
+++ toDoContextLocal/src/App.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Todoprovider } from './contexts'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
 const [todos,setTodos]=useState([])
@@ -20,8 +22,20 @@ const deleteTodo=(id)=>{
 }
 
 const toggleComplete=(id)=>{
-  setTodos((prev)=>prev.map((prevtodo)=>prevtodo===id?{...prevtodo,completed:!prevtodo.completed}:prevtodo))
+  setTodos((prev)=>prev.map((prevtodo)=>prevtodo.id===id?{...prevtodo,completed:!prevtodo.completed}:prevtodo))
 }
+
+useEffect(()=>{
+  const todos=JSON.parse(localStorage.getItem("todos"))//localStorage bs strings se deal krta hai
+
+  if(todos&& todos.length>0){
+    setTodos(todos)
+  }
+},[])
+
+useEffect(() => {
+  localStorage.setItem("todos",JSON.stringify(todos))
+}, [todos])
 
   return (
     <Todoprovider value={{todos,addTodo,updatedTodo,deleteTodo,toggleComplete}}>
@@ -29,10 +43,12 @@ const toggleComplete=(id)=>{
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                       <TodoForm/> {/* Todo form goes here */} 
                     </div>
                     <div className="flex flex-wrap gap-y-3">
-                        {/*Loop and Add TodoItem here */}
+                        {todos.map((todo)=>(
+                          <div key={todo.id}><TodoItem todo={todo}/></div>
+                        ))} {/*Loop and Add TodoItem here */}
                     </div>
                 </div>
             </div>
