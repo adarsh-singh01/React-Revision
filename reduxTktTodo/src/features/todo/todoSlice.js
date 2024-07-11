b646622cd6094d6cfc734d5e99696497b16467f8
Adarsh Singh <adarshsince2004@gmail.com>
2024-07-12 01:41:05 +0530

    todo by redux complete

---
+++ reduxTktTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,26 @@
+import { createSlice,nanoid } from "@reduxjs/toolkit";
+
+const initialState={
+    todos:[{id:1,text:"hello ji"}]
+}
+
+export const todoSlice=createSlice({
+    name:'todoApp',
+    initialState,
+    reducers:{//functions bahar bhi define krke yaha access de skte hai
+        addTodo:(state,action)=>{
+            const todo={
+                id:nanoid(),
+                text:action.payload//kyuki jisko access krenge uska naam bhi same hai isiliye payload.text nhi likha
+            }
+            state.todos.push(todo)
+        },
+        removeTodo:(state,action)=>{
+            state.todos=state.todos.filter((todo)=>todo.id!==action.payload)
+        }
+    }
+})
+
+export const {addTodo,removeTodo}=todoSlice.actions
+
+export default todoSlice.reducer
