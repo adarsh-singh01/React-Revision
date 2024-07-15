62c4eb30b122953a96fd5cdbbd2ec67a0eaf469b
Adarsh Singh <adarshsince2004@gmail.com>
2024-07-15 20:31:53 +0530

    about state

--- reduxTktTodo/src/features/todo/todoSlice.js
+++ reduxTktTodo/src/features/todo/todoSlice.js
@@ -6,14 +6,14 @@ const initialState={
 
 export const todoSlice=createSlice({
     name:'todoApp',
-    initialState,
+    initialState,//jab key aur value same ho to aese likh skte h
     reducers:{//functions bahar bhi define krke yaha access de skte hai
         addTodo:(state,action)=>{//state me jo abhi pura array h todos ka wo jayega
             const todo={
                 id:nanoid(),
                 text:action.payload//kyuki jisko access krenge uska naam bhi same hai isiliye payload.text nhi likha
             }
-            state.todos.push(todo)
+            state.todos.push(todo)//redux me stated preserve rehti h isliye humne aese kr liya nhi to context api ki tarah spread(...) use krna pdta
         },
         removeTodo:(state,action)=>{
             state.todos=state.todos.filter((todo)=>todo.id!==action.payload)
