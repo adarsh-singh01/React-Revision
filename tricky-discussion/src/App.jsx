66fdacb13c88fba9a99b5f23dd5b47dbff0c0f4e
Adarsh Singh <adarshsince2004@gmail.com>
2024-08-02 21:32:11 +0530

    tricky data type question

--- tricky-discussion/src/App.jsx
+++ tricky-discussion/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 function App() {
 
   //IN REACT JAB BHI STATE YA PROPS ME KOI CHANGE HOTA HAI TO COMPONENT RE-RENDER HOTA HAI
+
+  console.log("app rendered",Math.random());
   const [value,setValue]=useState(1)
   //const [mulVal,setMulVal]=useState(1)
 
@@ -10,6 +12,20 @@ function App() {
   const mulBy5=()=>{
     //setMulVal(value*5)
     setValue(value+1)//idk why but without this not working
+
+    //RENDER HOGA YA NHI K SCENARIO//btw shuru me ek baar to hota hi hai
+    setValue(1)//render nhi hoga kyuki pehle se value 1 hai
+    setValue(3)//ek baar bs render hoga 3 hone k liye phir ni hoga
+    
+    
+    //agar upar kuch aesa hota to phir 
+    /*const [value,setValue]=useState({
+        value:0
+    })*/
+    /*Tab agr aese likhte agr
+    setValue({
+      value:0
+    })*///Tab har baar render hota kyuki ye object hai...ye memory reference leta hai isliye har baar change hota hai
   }
   return (
     <div>
